f3fe1d9556595f09dc5f50be3c226e783ab85a2d
fernandoteixeira <fernandoadt1@gmail.com>
2019-11-06 13:43:50 -0300

    Making webdriverIO Async

--- specs/VisualAITests.js
+++ specs/VisualAITests.js
@@ -7,24 +7,47 @@ const {
     Eyes,
     Target
 } = require('@applitools/eyes-webdriverio');
-let eyes = new Eyes();
+const {
+    Configuration
+} = require('@applitools/eyes-selenium');
+//let eyes = new Eyes();
+let eyes;
 var viewportSize;
-eyes.setApiKey("9aUfuVNrcUdWHGqDE1Sn102EHQKnjS4DdbhVySEn61Azg110")
+//eyes.setApiKey("9aUfuVNrcUdWHGqDE1Sn102EHQKnjS4DdbhVySEn61Azg110")
 
 describe('Test Scenarios using Applitools Eyes', () => {
 
-
-    beforeEach(() => {
+    beforeEach(async () => {
           browser.url(browser.options.baseUrl)
           browser.maximizeWindow();
           viewportSize = browser.getWindowSize()
     });
 
+     afterEach(async () => {
+         await eyes.abortIfNotClosed();
+
+         const results = await eyes.getRunner().getAllTestResults(true);
+         console.log(results);
+         console.log(results.getAllResults());
+     });
+
     describe('Scenario 1 - Login Page UI Elements Test', function () {
 
         it('All the expected elements should exist on the page', async function () {
+            const runner = new ClassicRunner();
+
+            eyes = new Eyes(runner);
+
+            const configuration = new Configuration();
+            configuration.setAppName('Applitools Hackaton');
+            configuration.setTestName('Scenario 1 - Login Page UI Elements Test');
+            configuration.setApiKey("9aUfuVNrcUdWHGqDE1Sn102EHQKnjS4DdbhVySEn61Azg110");
+            configuration.setBatch('Scenario 1 - Login Page UI Elements Test');
+            eyes.setConfiguration(configuration);
+
             try{
-                await eyes.open(browser,'Applitools Hackaton', 'Scenario 1 - Login Page UI Elements Test', viewportSize);
+                //await eyes.open(browser,'Applitools Hackaton', 'Scenario 1 - Login Page UI Elements Test', viewportSize);
+                await eyes.open(browser);
                 await eyes.check('Login Window', Target.window());
                 await eyes.closeAsync();
             }
@@ -42,32 +65,60 @@ describe('Test Scenarios using Applitools Eyes', () => {
             ['', '12345', 'No Username'],
             ['fernando', '12345', 'Username and Password filled']
         ];
+
+        const runner = new ClassicRunner();
+
+        eyes = new Eyes(runner);
+
+        const configuration = new Configuration();
+        configuration.setAppName('Applitools Hackaton');
+        configuration.setTestName('Scenario 2 - Data - Driven Test');
+        configuration.setApiKey("9aUfuVNrcUdWHGqDE1Sn102EHQKnjS4DdbhVySEn61Azg110");
+        eyes.setConfiguration(configuration);
         //eyes.setBatch('Scenario 2 - Data - Driven Test');
-        userCredencialsDataDriven.forEach(function (userCredencials) {
+        //userCredencialsDataDriven.forEach( async function (userCredencials) {
             it('Sending the credentials', function () {
-                $('#username').setValue(userCredencials[0])
-                $('#password').setValue(userCredencials[1])
-                $('#log-in').click()
+                const usernameField = await $('#username');
+                 usernameField.setValue(userCredencialsDataDriven[0][0])
+                 const passwordField = await $('#password');
+                 passwordField.setValue(userCredencialsDataDriven[1][1]);
+                 const LoginButton = await $('#log-in');
+                 LoginButton.click()
+
                 it('Validating using applitools', async function () {
-                    try {
-                        await eyes.open(browser, 'Applitools Hackaton', 'Scenario 2 - Data-Driven Test' + userCredencials[3], viewportSize);
-                        if (userCredencials[0] != "" && userCredencials[1] != "") {
-                            //successful login and Main Screen displayed
-                            await eyes.check('Main Screen Window', Target.window());
-                            await eyes.closeAsync();
-                        } else if (userCredencials[0] === "" || userCredencials[1] === "") {
-                            //An error happened and a message was displayed
-                            await eyes.check('Error alert displayed on Login Screen', Target.window());
-                            await eyes.closeAsync();
-                        }
-                    } finally {
-                        await eyes.abortIfNotClosed();
-                    }
+                    await eyes.open(browser);
+                    await eyes.check('Main Screen Window', Target.window());
+                    await eyes.closeAsync();
                 })
             })
-        })
+        //})
     })
     
+
+    describe('Scenario 3 - Test', function () {
+
+        it('All the expected elements should exist on the page', async function () {
+            const runner = new ClassicRunner();
+
+            eyes = new Eyes(runner);
+
+            const configuration = new Configuration();
+            configuration.setAppName('Applitools Hackaton');
+            configuration.setTestName('Scenario 3 - Test');
+            configuration.setApiKey("9aUfuVNrcUdWHGqDE1Sn102EHQKnjS4DdbhVySEn61Azg110");
+            configuration.setBatch('Scenario 3 - Test');
+            eyes.setConfiguration(configuration);
+
+            try {
+                await eyes.open(browser);
+                await eyes.check('Login Window', Target.window());
+                await eyes.closeAsync();
+            } finally {
+                await eyes.abortIfNotClosed();
+            }
+        })
+    })
+
 })
 
 
